6d4868c76651d3284efec4343c76a95a76c4db97
Varun Santhanam <talkto@vsanthanam.com>
2021-06-13 09:22:30 -0700

    Initial Commit

---
+++ Sources/AppFoundation/AppFoundation.swift
@@ -0,0 +1,3 @@
+struct AppFoundation {
+    var text = "Hello, World!"
+}
---
+++ Tests/AppFoundationTests/AppFoundationTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import AppFoundation
+
+    final class AppFoundationTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(AppFoundation().text, "Hello, World!")
+        }
+    }
