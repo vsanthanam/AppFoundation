51c58c7e5cfa1e28b5c6248a92f2b91b71a727c4
Varun Santhanam <talkto@vsanthanam.com>
2021-11-20 12:28:18 -0800

    Fix asyncMain
    Summary: Fix `asyncMain` impl
    
    Test Plan: n/a
    
    Differential Revision: https://code.vsanthanam.com/D475

--- Sources/AppFoundation/Globs.swift
+++ Sources/AppFoundation/Globs.swift
@@ -7,6 +7,6 @@ import Foundation
 
 /// Asynchronously excute code on the main thread
 /// - Parameter closure: The code to exeucute on the main thread
-public func asyncMain(_ closure: @escaping @Sendable () -> Void) {
-    Task { await MainActor.run(body: closure) }
+public func asyncMain(_ closure: @MainActor @Sendable @escaping () -> Void) {
+    Task.detached { await MainActor.run(body: closure) }
 }
