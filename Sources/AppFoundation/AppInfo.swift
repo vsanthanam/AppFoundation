943b299cfffb75d61693dcb21a4376582c541916
Varun Santhanam <talkto@vsanthanam.com>
2021-11-20 12:30:03 -0800

    Update AppInfo

--- Sources/AppFoundation/AppInfo.swift
+++ Sources/AppFoundation/AppInfo.swift
@@ -8,12 +8,12 @@ import Foundation
 public enum AppInfo {
 
     /// The app version number from info.plist
-    static var version: String {
+    public static var version: String {
         Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
     }
 
     /// The app build number from info.plist
-    static var build: String {
+    public static var build: String {
         Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
     }
 
