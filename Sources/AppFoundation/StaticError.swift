b46c9d7b18df5a15d8ff9328f44025361089d171
Varun Santhanam <talkto@vsanthanam.com>
2022-06-27 19:06:45 -0700

    Fix Compilation Issues in StaticError.swift

--- Sources/AppFoundation/StaticError.swift
+++ Sources/AppFoundation/StaticError.swift
@@ -23,6 +23,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import Foundation
+
 /// A reusable swift error struct
 public struct StaticError: LocalizedError, Equatable, Hashable {
 
